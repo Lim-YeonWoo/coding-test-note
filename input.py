09bce83cdd9e9f7d85f577abc53d7bc5167a992b
Lim Yeon Woo <90602936+Lim-YeonWoo@users.noreply.github.com>
2023-07-03 20:45:00 -0800

    Update input.py

--- input.py
+++ input.py
@@ -6,17 +6,26 @@
 10
 3 5 7
 12 34 56 67 56
+123456789
 '''
+
+# 방법1
 a = int(input()) # 8
 b = int(input()) # 10
 
+# 방법2
 x, y, z = map(int, input().split()) # 3, 5, 7
 
+# 방법3
 array_name = list(map(int, input().split())) # [12, 34, 56, 67, 56]
+
+# 방법4 (input()이 속도가 느리다)
+import sys
+c = sys.stdin.readline().rstrip() # 123456789
 
 print(a)
 print(b)
 print(x)
 print(y)
 print(z)
-print(array_name)
+print(c)
