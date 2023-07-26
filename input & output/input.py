097dc47ad1d1a0902ae96caca577197a1bf57ddb
Lim Yeon Woo <90602936+yeonvvoo@users.noreply.github.com>
2023-07-26 11:15:41 +0900

    sys.stdin.readline().rstrip()

--- input & output/input.py
+++ input & output/input.py
@@ -20,8 +20,18 @@ x, y, z = map(int, input().split()) # 3, 5, 7
 array_name = list(map(int, input().split())) # [12, 34, 56, 67, 56]
 
 # 방법4 (input()이 속도가 느리다)
+'''
 import sys
 c = sys.stdin.readline().rstrip() # 123456789
+
+위의 방법이 맞긴한데 귀찮으니까 아래처럼 이용
+input().rstrip()이 input()하나 인 것처럼 쓰기
+'''
+
+import sys
+input = sys.stdin.readline
+c = input().rstrip()
+d, e = input().rstrip().split()
 
 print(a)
 print(b)
